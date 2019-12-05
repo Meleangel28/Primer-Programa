1c6ca6d134661b5e16aa9d8aa32214947cb7dceb
Angel Melean <angelalmelean@gmail.com>
2019-12-05 19:46:30 -0400

    Archivos basicos que tenia de prueba. Explicaciones de temas sencillos y necesarios.

--- test_one.py
+++ test_one.py
@@ -10,10 +10,13 @@ print('La edad de Angel es', edad_angel, 'y nacio en', agno_nacimiento, '.')
 number_to_guess = 4
 attemps = 5
 while attemps != 0:
+    print("Le quedan {} intentos.".format(attemps))
     user_number = int(input("Adivina un numero: "))
     if user_number == number_to_guess:
         print("Usted gano la adivinanza.")
         break
     elif user_number != number_to_guess:
-        attemps -= 1
-        print("No adivino el numero, tiene un intento menos.")
+        attemps = attemps - 1
+        if attemps == 0:
+            print("Usted se ha quedado sin intentos.")
+            break
