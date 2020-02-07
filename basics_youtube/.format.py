56dabc9beb3e733c371335ac60e93a78b9b3d898
Angel Melean <angelalmelean@gmail.com>
2020-02-07 13:44:46 -0400

    Archivos basicos que tenia de prueba. Explicaciones de temas sencillos y necesarios.

---
+++ basics_youtube/.format.py
@@ -0,0 +1,4 @@
+from math import sqrt
+x = 4
+
+print("{0:2}{0:3d}  {0:3.2f}".format(x, x**2, sqrt(x)))
---
+++ basics_youtube/Random.py
@@ -0,0 +1,10 @@
+import random
+
+#Numero aleatorio natural de 1 al 99
+print("", random.randint(1,100))
+
+#Numero aleatorio real de 1 al 99
+print("", random.uniform(1, 99))
+
+#Numero aleatorio entre un rango de valores establecidos.
+print("", random.randrange(0, 100, 2))
