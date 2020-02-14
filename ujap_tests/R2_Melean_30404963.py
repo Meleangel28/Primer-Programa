43ea41ac044c5b82d4f40e1a990a6cb0b3e844cf
Angel Melean <angelalmelean@gmail.com>
2020-02-14 17:55:56 -0400

    Avances.

--- ujap_tests/R2_Melean_30404963.py
+++ ujap_tests/R2_Melean_30404963.py
@@ -1,92 +1,44 @@
-# Libreria de matematicas.
-from math import sqrt
+#Inicializacion de variables.
+"""año de nacimiento"""
+cedula = ano_nacimiento  = 0
+ano_dosprimerosdigitos = 2
+N1 = N2 = N3 = CODIGO = 0
+temp = 0.0
+temp2 = ""
 
-def d(X2, X1, Y2, Y1):
-    d = sqrt(((X2 - X1)**2) + ((Y2 - Y1)**2))
-    return d
+#Entrada de datos.
+cedula = int(input("Ingrese la cedula del empleado: "))
+ano_nacimiento = int(input("Ingrese año de nacimiento del empleado: "))
+print("Procesando... \n")
 
-# Declaracion de variables.
-X1 = X2 = X3 = 0.0
-Y1 = Y2 = Y3 = 0.0
-d_lado_uno = d_lado_dos = d_lado_tres = 0.0 # Distancia de cada uno de los lados.
-cateto_uno = cateto_dos = hipotenusa_calculada = hipotenusa_real = 0.0
-punto_uno = punto_dos = punto_tres = (0,0)
+temp = ano_nacimiento
+# Primeros dos digitos del codigo, dos primeros digitos del ano.
+for i in range(0, 2):
+    N1 = temp // 10
+    temp = N1
+str(N1)
 
-#Entrada del usuario
-X1 = int(input("Ingrese X de P1: "))
-Y1 = int(input("Ingrese Y de P1: "))
+temp = ano_nacimiento
+#Ultimos dos digitos del codigos, dos ultimos numeros de ano.
+for i in range (0, 2):
+    N3 = temp % 10 #6
+    temp2 = str(N3) + str(temp2)
+    temp = temp // 10
+N3 = temp2
+str(N3)
 
-X2 = int(input("\nIngrese X de P2: "))
-Y2 = int(input("Ingrese Y de P2: "))
+#Cuatro primeros digitos impares y/o completar con 0
+temp = 0
+temp2 = ""
+for i in range(0, cedula):
+    temp = cedula % 10
+    if temp == 1 or temp == 3 or temp == 7 or temp == 9:
+        temp2 = temp2 + str(temp)
+    cedula = cedula //10
+for i in range(len(temp2), 4):
+    temp2 = temp2 + "0"
+N2 = temp2
 
-X3 = int(input("\nIngrese X de P3: "))
-Y3 = int(input("Ingrese Y de P3: "))
-
-punto_uno = (X1, Y1)
-punto_dos = (X2, Y2)
-punto_tres = (X3, Y3)
-
-print("El punto uno es {}".format(punto_uno))
-print("El punto dos es {}".format(punto_dos))
-print("El punto tres es {}".format(punto_tres))
-
-# Procedimiento.
-# Distancia entre puntos.
-
-d_lado_uno = d(X2, X1, Y2, Y1)
-d_lado_dos = d(X3, X2, Y3, Y2)
-d_lado_tres = d(X3,X1, Y2, Y1)
-
-print("\nLa distancia del lado uno es: {} ".format(d_lado_uno))
-print("La distancia del lado dos es: {} ".format(d_lado_dos))
-print("La distancia del lado tres es: {} ".format(d_lado_tres))
-
-# Determinar cual es la hipotenusa (Distancia mayor = Hipotenusa)
-if (d_lado_tres > d_lado_dos) and (d_lado_tres > d_lado_uno):
-    hipotenusa_calculada = d_lado_tres
-elif (d_lado_dos > d_lado_uno) and (d_lado_dos > d_lado_tres):
-    hipotenusa_calculada = d_lado_dos
-elif (d_lado_uno > d_lado_dos) and (d_lado_uno > d_lado_tres):
-    hipotenusa_calculada = d_lado_uno
-
-
-if d_lado_uno == hipotenusa_calculada:
-    cateto_uno = d_lado_dos
-    cateto_dos = d_lado_tres
-elif d_lado_dos == hipotenusa_calculada:
-    cateto_uno = d_lado_uno
-    cateto_dos = d_lado_tres
-elif d_lado_tres == hipotenusa_calculada:
-    cateto_uno = d_lado_uno
-    cateto_dos = d_lado_dos
-
-print("\nLa hipotenusa calculada tiene una distancia de: {}".format(hipotenusa_calculada))
-print("El cateto uno tiene una distancia de: {}".format(cateto_uno))
-print("El cateto dos tiene una distancia de: {}".format(cateto_dos))
-
-# Determinar hipotenusa real y decir que tipo de triangulo es.
-hipotenusa_real = sqrt(((cateto_uno)**2)+((cateto_dos)**2))
-print("\nLa hipotenusa real es de: {}".format(hipotenusa_real))
-
-if hipotenusa_calculada == hipotenusa_real:
-    print("Debido a que la hipotenusa calculada y la hipotenusa real son iguales entonces:")
-    print("Es un triangulo rectangulo.")
-    if cateto_uno == cateto_dos:
-        print("Y debido a que ambos catetos son iguales entonces: ")
-        print("Es un triangulo rectangulo isosceles.")
-    else:
-        print("Y debido a que ambos catetos no tienen el mismo valor entonces: ")
-        print("Es un triangulo rectangulo escaleno.")
-else:
-    print("Debido a que la hipotenusa calculada no es igual a la hipotenusa real entonces: ")
-    print("No es un triangulo rectangulo.")
-
+CODIGO = (str(N1) + str(N2) + str(N3))
+print("Codigo del empleado: {}".format(CODIGO))
 print("Programa finalizado.")
-
-
-
-
-
-
-
-
